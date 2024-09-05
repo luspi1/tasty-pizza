f22105a2aad432b8eaf4d0863428bf5327e8e091
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-09-05 17:45:49 +0300

    расширение seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { hashSync } from 'bcrypt'
 import { PrismaClient } from '@prisma/client'
+import { _ingredients, categories, products } from '@/prisma/const'
 const prisma = new PrismaClient()
 
 async function up() {
@@ -23,29 +24,63 @@ async function up() {
 	})
 
 	await prisma.category.createMany({
-		data: [
-			{
-				name: 'Пиццы'
-			},
-			{
-				name: 'Завтрак'
-			},
-			{
-				name: 'Закуски'
-			},
-			{
-				name: 'Коктейли'
-			},
-			{
-				name: 'Напитки'
-			},
-		]
+		data: categories,
 	})
+
+	await prisma.ingredient.createMany({
+		data: _ingredients,
+	})
+
+	await prisma.product.createMany({
+		data: products
+	})
+
+	const pizza1 = await prisma.product.create({
+		data: {
+			name: 'Пепперони фреш',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: [],
+			},
+		},
+	});
+
+	const pizza2 = await prisma.product.create({
+		data: {
+			name: 'Сырная',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: _ingredients.slice(5, 10),
+			},
+		},
+	});
+
+	const pizza3 = await prisma.product.create({
+		data: {
+			name: 'Чоризо фреш',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: _ingredients.slice(10, 40),
+			},
+		},
+	});
 
 }
 
 async function down() {
-	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
 }
 
 async function main() {
