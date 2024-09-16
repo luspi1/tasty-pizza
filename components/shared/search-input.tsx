19b8dfbfc23db9a4daa83569a3fc3727720932ab
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-09-16 15:23:40 +0300

    рабочий поиск

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,54 +1,58 @@
-'use client';
+'use client'
 
 import React, { FC, useRef, useState } from 'react'
-import { Product } from '@prisma/client';
-import { Search } from 'lucide-react';
-import Link from 'next/link';
-import { useClickAway, useDebounce } from 'react-use';
+import { Product } from '@prisma/client'
+import { Search } from 'lucide-react'
+import Link from 'next/link'
+import { useClickAway, useDebounce } from 'react-use'
 
 import { cn } from '@/lib/utils'
+import { Api } from '@/services/api-client'
 
 interface Props {
 	className?: string;
 }
 
 export const SearchInput: FC<Props> = ({ className }) => {
-	const [searchQuery, setSearchQuery] = useState('');
-	const [focused, setFocused] = useState(false);
-	const [products, setProducts] = useState<Product[]>([]);
-	const ref = useRef(null);
+	const [searchQuery, setSearchQuery] = useState('')
+	const [focused, setFocused] = useState(false)
+	const [products, setProducts] = useState<Product[]>([])
+	const ref = useRef(null)
 
 	useClickAway(ref, () => {
-		setFocused(false);
-	});
+		setFocused(false)
+	})
 
-	// useDebounce(
-	// 	async () => {
-	// 		try {
-	// 			const response = await Api.products.search(searchQuery);
-	// 			setProducts(response);
-	// 		} catch (error) {
-	// 			console.log(error);
-	// 		}
-	// 	},
-	// 	250,
-	// 	[searchQuery],
-	// );
+	useDebounce(
+		async () => {
+			try {
+				const response = await Api.products.search(searchQuery)
+				setProducts(response)
+			} catch (error) {
+				console.log(error)
+			}
+		},
+		250,
+		[searchQuery],
+	)
 
 	const onClickItem = () => {
-		setFocused(false);
-		setSearchQuery('');
-		setProducts([]);
-	};
+		setFocused(false)
+		setSearchQuery('')
+		setProducts([])
+	}
+
 
 	return (
 		<>
-			{focused && <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30" />}
+			{focused &&
+				<div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30"/>}
 
 			<div
 				ref={ref}
 				className={cn('flex rounded-2xl flex-1 justify-between relative h-11 z-30', className)}>
-				<Search className="absolute top-1/2 translate-y-[-50%] left-3 h-5 text-gray-400" />
+				<Search
+					className="absolute top-1/2 translate-y-[-50%] left-3 h-5 text-gray-400"/>
 				<input
 					className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
 					type="text"
@@ -58,7 +62,7 @@ export const SearchInput: FC<Props> = ({ className }) => {
 					onChange={(e) => setSearchQuery(e.target.value)}
 				/>
 
-				{products.length > 0 && (
+				{products?.length > 0 && (
 					<div
 						className={cn(
 							'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
@@ -70,7 +74,8 @@ export const SearchInput: FC<Props> = ({ className }) => {
 								key={product.id}
 								className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
 								href={`/product/${product.id}`}>
-								<img className="rounded-sm h-8 w-8" src={product.imageUrl} alt={product.name} />
+								<img className="rounded-sm h-8 w-8" src={product.imageUrl}
+								     alt={product.name}/>
 								<span>{product.name}</span>
 							</Link>
 						))}
@@ -78,5 +83,5 @@ export const SearchInput: FC<Props> = ({ className }) => {
 				)}
 			</div>
 		</>
-	);
-};
+	)
+}
--- services/products.ts
+++ services/products.ts
@@ -1,3 +1,7 @@
-export const search = async (query: string) => {
+import { axiosInstance } from '@/services/instance'
+import { Product } from '@prisma/client'
+import { ApiRoutes } from '@/services/const'
 
+export const search = async (query: string) => {
+	return (await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {params: {query}})).data
 }
